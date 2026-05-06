eaf672f53430f7b1b96933fbc05f6a4e61dc829d
huwenjie912 <huwenjie912@gmail.com>
2026-05-06 22:29:04 +0800

    fix hydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${display.variable} ${body.variable}`}>
+    <html lang="en" className={`${display.variable} ${body.variable}`} suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: themeInit }} />
       </head>
